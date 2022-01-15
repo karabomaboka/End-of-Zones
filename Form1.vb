94accdd5800adb5f3182d3112710c063ce125697
Karabo Maboka <51972141+karabomaboka@users.noreply.github.com>
2022-01-15 17:16:54 +0200

    Add files via upload

---
+++ Form1.vb
@@ -0,0 +1,10 @@
+﻿Public Class Form1
+  Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+    Dim fmtStr As String = "{0, 5}  {1, -5}" 'Two spaces after the
+    '                                        first right curly bracket
+    lstOutput.Items.Add("12345678901234567890")
+    lstOutput.Items.Add(String.Format(fmtStr, 1, 2))
+
+  End Sub
+End Class
